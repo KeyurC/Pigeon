89c547bb115af4cbf04a7a7266282e023cddeacc
Keyur Canji <Keyurschool8@gmail.com>
2020-09-19 16:27:24 +0100

    Package for spring application

---
+++ PigeonMessenger/src/main/java/Spring/Controller.java
@@ -0,0 +1,15 @@
+package Spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/test")
+    public String test(@RequestParam(value = "name",defaultValue = "fail") String name) {
+        return name;
+    }
+
+}
