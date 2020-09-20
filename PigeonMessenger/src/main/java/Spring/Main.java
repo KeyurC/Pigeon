d3beaf2132b2418d9b22b22b8dc94e3dacc66670
Keyur Canji <Keyurschool8@gmail.com>
2020-09-20 18:14:17 +0100

    Instantiated DBCon class to establish db connection

--- PigeonMessenger/src/main/java/Spring/Main.java
+++ PigeonMessenger/src/main/java/Spring/Main.java
@@ -1,5 +1,6 @@
 package Spring;
 
+import Hibernate.DBConnection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
+        DBConnection con = new DBConnection();
     }
 }
