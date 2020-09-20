fc9458222a1cd1e9d0f0b718e338624cb5c86b8a
Keyur Canji <Keyurschool8@gmail.com>
2020-09-20 18:15:29 +0100

    renamed from Controller, due to naming conflicts

--- PigeonMessenger/src/main/java/Spring/SpringController.java
+++ PigeonMessenger/src/main/java/Spring/SpringController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class SpringController {
 
     @GetMapping("/test")
     public String test(@RequestParam(value = "name",defaultValue = "fail") String name) {
