e9474ee5bb72737580bdef22e9506f76163b5ada
Keyur Canji <Keyurschool8@gmail.com>
2020-09-20 17:42:25 +0100

    Implemented DBConnection class, started db connection and created static session and transaction for DAO

--- PigeonMessenger/src/main/java/Hibernate/DBConnection.java
+++ PigeonMessenger/src/main/java/Hibernate/DBConnection.java
@@ -1,5 +1,28 @@
 package Hibernate;
 
+import Model.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
 public class DBConnection {
+
+    private static Transaction transaction;
+    private static Session session;
+
+    public static Transaction getTransaction() {
+        return transaction;
+    }
+
+    public static Session getSession() {
+        return session;
+    }
+
+    public DBConnection() {
+        session = new Configuration().
+                configure().buildSessionFactory().openSession();
+        transaction = session.beginTransaction();
+    }
 
 }
