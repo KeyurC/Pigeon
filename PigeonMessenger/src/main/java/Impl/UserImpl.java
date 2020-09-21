93eb46174a0cda7e51acc870d18d03551627d66a
Keyur Canji <Keyurschool8@gmail.com>
2020-09-21 12:59:05 +0100

    User Implementation class to use UserDao to perform user tasks e.g create user

---
+++ PigeonMessenger/src/main/java/Impl/UserImpl.java
@@ -0,0 +1,28 @@
+package Impl;
+
+import DAO.Dao;
+import DAO.UserDao;
+import Model.User;
+
+public class UserImpl {
+
+    /**
+     * Creates a new User object and saves the user to
+     * the database using the DAO.
+     * @param name
+     */
+    public static void createUser(String name) {
+        Dao userDao = new UserDao();
+        User user = new User();
+
+        user.setUser_name(name);
+        userDao.save(user);
+
+
+    }
+
+    static void deleteUser() {
+
+    }
+
+}
