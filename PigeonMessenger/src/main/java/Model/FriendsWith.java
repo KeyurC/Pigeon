86432a55feac6ecd669944c9ccc892403c7e8696
Keyur Canji <Keyurschool8@gmail.com>
2020-09-21 14:29:32 +0100

    added annotations onto object for hibernate
    failed to recongize as hibernate object due to lack of annotations hence object could not be saved to db

--- PigeonMessenger/src/main/java/Model/FriendsWith.java
+++ PigeonMessenger/src/main/java/Model/FriendsWith.java
@@ -1,9 +1,43 @@
 package Model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "FriendsWith")
 public class FriendsWith {
+
+    @Id
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "UserID")
     private int userID;
+
+    @Column(name = "FriendID")
     private int friendID;
-    private Boolean friendStatus;
+
+    @Column(name = "status")
+    private int friendStatus;
+
+    public int getFriendID() {
+        return friendID;
+    }
+
+    public void setFriendID(int friendID) {
+        this.friendID = friendID;
+    }
+
+    public int getFriendStatus() {
+        return friendStatus;
+    }
+
+    public void setFriendStatus(int friendStatus) {
+        this.friendStatus = friendStatus;
+    }
 
     public int getUserID() {
         return userID;
