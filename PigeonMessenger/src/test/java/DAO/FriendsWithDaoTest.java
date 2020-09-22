39f521cc301c84d85c3a6acdc6876bcfc321ca9e
Keyur Canji <Keyurschool8@gmail.com>
2020-09-22 13:26:32 +0100

    fixed tests to generate db components and pass first time

--- PigeonMessenger/src/test/java/DAO/FriendsWithDaoTest.java
+++ PigeonMessenger/src/test/java/DAO/FriendsWithDaoTest.java
@@ -1,52 +1,80 @@
 package DAO;
 
+import DAO.DAOImpl.FriendsWithDao;
+import DAO.DAOImpl.UserDao;
 import Hibernate.DBConnection;
 import Model.FriendsWith;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import Model.User;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FriendsWithDaoTest {
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() throws InterruptedException {
         new DBConnection();
+        User user = new User();
+        User user1 = new User();
+        UserDao userDao = new UserDao();
+
+        if (userDao.get(1) == null) {
+            user.setUser_name("test");
+            userDao.save(user);
+        }
+        if (userDao.get(2) == null) {
+            user1.setUser_id(2);
+            user1.setUser_name("test_name");
+            userDao.save(user1);
+        }
+
     }
 
-
     @Test
+    @Order(1)
     void save() {
-//        Dao dao = new FriendsWithDao();
-//        FriendsWith request = new FriendsWith();
-//
-//        request.setUserID(1);
-//        request.setFriendID(2);
-//        request.setFriendStatus(0);
-//        dao.save(request);
+        FriendsWithDao dao = new FriendsWithDao();
+        FriendsWith request = new FriendsWith();
+
+        request.setUserID(1);
+        request.setFriendID(2);
+        request.setFriendStatus(0);
+        assertDoesNotThrow(() -> dao.save(request),
+                "No throw means object saved successfully");
     }
 
     @Test
+    @Order(2)
     void get() {
     }
 
+    @Order(3)
     @Test
     void getByFriends() {
         FriendsWithDao dao = new FriendsWithDao();
-        FriendsWith friend = dao.getByFriends(3,2);
-        System.out.println(friend.getUserID());
+        FriendsWith friend = dao.getByFriends(1,2);
+        int userID = friend.getUserID();
+        assertEquals(userID,1,
+                "Pass means integrity of object is confirmed");
     }
 
 
     @Test
+    @Order(4)
     void update() {
         FriendsWithDao dao = new FriendsWithDao();
         FriendsWith friend = dao.get(1);
         friend.setFriendStatus(1);
         dao.update(friend);
 
+        friend = dao.get(1);
+        System.out.println(friend.getUserID());
+        assertEquals(friend.getFriendStatus(),1);
+
     }
 
+    @Order(5)
     @Test
     void delete() {
     }
