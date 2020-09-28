d84f69729f58e92efd2cf2ba257004bea20c1522
Keyur Canji <Keyurschool8@gmail.com>
2020-09-28 18:26:40 +0100

    Dynamically adds friend components and passes on friend data on props

--- PigeonMessenger/src/main/frontend/src/Components/Messenger/Friends.jsx
+++ PigeonMessenger/src/main/frontend/src/Components/Messenger/Friends.jsx
@@ -1,10 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Friend from "./Friend";
+
+import "./Messenger.css";
 
 class Friends extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+  state = {
+    friendList: [
+      { id: 1, name: "test", lastMessage: "text" },
+      { id: 2, name: "tes2t_name", lastMessage: "text" }
+    ]
+  };
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="messaging">
+          <div className="inbox_msg">
+            <div className="inbox_people">
+              <div className="headind_srch"></div>
+              <div className="inbox_chat">
+                  {this.state.friendList.map(friend => (
+                    <Friend key={friend.id} name={friend.name} msg={friend.lastMessage}/>
+                  ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Friends;
