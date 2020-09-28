eb66984e27fd01b9c72f95c6cabb32a85a720633
Keyur Canji <Keyurschool8@gmail.com>
2020-09-28 18:25:20 +0100

    updated jsx layout and provided attributes for friend
    added css and jsx to form a friend grid chat and used props to send basic information about friends e.g name

--- PigeonMessenger/src/main/frontend/src/Components/Messenger/Friend.jsx
+++ PigeonMessenger/src/main/frontend/src/Components/Messenger/Friend.jsx
@@ -1,10 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import "./Messenger.css"
 
 class Friend extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className="chat_list">
+          <div className="chat_people">
+              <div className="chat_ib">
+                <h5>
+                  {this.props.name}<span className="chat_date">Dec 25</span>
+                </h5>
+                <p>
+                  {this.props.msg}
+                </p>
+              </div>
+            </div>
+          </div>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Friend;
