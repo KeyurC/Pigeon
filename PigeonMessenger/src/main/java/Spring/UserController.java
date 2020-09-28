c0b9511a20cb26079efd1fc0ea538a2c705c4404
Keyur Canji <Keyurschool8@gmail.com>
2020-09-28 15:50:46 +0100

    updated UserController to accept data sent by react
    replaced ModelAttribute tag as thymeleaf is no longer being used and parsed Json data

--- PigeonMessenger/src/main/java/Spring/UserController.java
+++ PigeonMessenger/src/main/java/Spring/UserController.java
@@ -1,14 +1,17 @@
 package Spring;
 
 import Impl.UserImpl;
+import org.apache.log4j.Logger;
 import Model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
+
+    private static final Logger logger = Logger.getLogger(UserController.class);
 
     @GetMapping(value = "/")
     public String index(Model model){
@@ -18,9 +21,18 @@ public class UserController {
 
     @RequestMapping(value = "/createUser", method = RequestMethod.POST)
     @ResponseBody
-    public String userCreation(@ModelAttribute User user) {
-        System.out.println(" createUserMethod " + user.getUser_name());
-        UserImpl.createUser(user);
-        return user.getUser_name();
+    public String userCreation(@RequestBody String userJSON) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            User user = mapper.readValue(userJSON,User.class);
+            UserImpl.createUser(user);
+            return "Success";
+        } catch (Exception e) {
+            logger.error("userCreation: failed to map JsonObject to User object", e);
+            return null;
+        }
+
+
     }
 }
