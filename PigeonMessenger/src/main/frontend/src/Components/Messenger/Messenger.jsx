b98d54da129c45b083df3e42e79ce34c1f09fd66
Keyur Canji <Keyurschool8@gmail.com>
2020-09-28 18:27:23 +0100

    added friends component to parent component

--- PigeonMessenger/src/main/frontend/src/Components/Messenger/Messenger.jsx
+++ PigeonMessenger/src/main/frontend/src/Components/Messenger/Messenger.jsx
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Friends from "./Friends";
+
+import "./Messenger.css";
 
 class Messenger extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container-fluid p-0 m-0">
+          <Friends />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Messenger;
