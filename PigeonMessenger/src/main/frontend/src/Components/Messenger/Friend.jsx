aafed9fefc4fa740841b78a9b9752c40abfea34d
Keyur Canji <Keyurschool8@gmail.com>
2020-09-30 13:43:37 +0100

    added a friend add icon to people who arent friends and an addFriend from props

--- PigeonMessenger/src/main/frontend/src/Components/Messenger/Friend.jsx
+++ PigeonMessenger/src/main/frontend/src/Components/Messenger/Friend.jsx
@@ -1,24 +1,37 @@
 import React, { Component } from "react";
-
-import "./Messenger.css"
+import { FaGripLinesVertical, FaUserPlus } from "react-icons/fa";
+import "./Messenger.css";
 
 class Friend extends Component {
   state = {};
+
+  addFriendIconHandler() {
+    if (this.props.isFriend !== 1) {
+      return (
+        <React.Fragment>
+          <FaUserPlus />
+        </React.Fragment>
+      );
+    }
+  }
+
+
+
   render() {
     return (
       <React.Fragment>
         <div className="chat_list">
           <div className="chat_people">
-              <div className="chat_ib">
-                <h5>
-                  {this.props.name}<span className="chat_date">Dec 25</span>
-                </h5>
-                <p>
-                  {this.props.msg}
-                </p>
-              </div>
+            <div className="chat_ib">
+              <h5>
+                {this.props.name}
+                <span onClick={() => this.props.onAddFriend(this.props.id)} className="chat_date">{this.addFriendIconHandler()}</span>
+              </h5>
+              {/* <p>{this.props.msg}</p> */}
+              <p>Test</p>
             </div>
           </div>
+        </div>
       </React.Fragment>
     );
   }
