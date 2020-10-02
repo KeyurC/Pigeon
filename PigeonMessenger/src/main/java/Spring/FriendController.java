2c1c6149fb62cb9bf0f4e55a4778917d6a91bc0a
Keyur Canji <Keyurschool8@gmail.com>
2020-10-02 15:06:31 +0100

    added accept/decline friendrequest methods

--- PigeonMessenger/src/main/java/Spring/FriendController.java
+++ PigeonMessenger/src/main/java/Spring/FriendController.java
@@ -2,33 +2,66 @@ package Spring;
 
 import DAO.DAOImpl.FriendsWithDao;
 import DAO.DAOImpl.UserDao;
+import Impl.FriendImpl;
+import Model.FriendsWith;
 import Model.User;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class FriendController {
-
+    private static FriendsWithDao friendsDAO = new FriendsWithDao();
+    private static UserDao userDAO = new UserDao();
     private static Logger logger = Logger.getLogger(FriendController.class);
+
+    @RequestMapping(value = "/acceptRequest", method = RequestMethod.POST)
+    @ResponseBody
+    public void acceptRequest(@RequestBody JsonNode json) {
+        JsonNode params = json.get("params");
+        String username = params.get("username").textValue();
+        String friendID = params.get("friendID").toString();
+
+        int userID = userDAO.getBasedOnUsername(username).getUser_id();
+        FriendImpl.acceptRequest(userID,Integer.valueOf(friendID));
+    }
+
+    @RequestMapping(value = "/declineRequest", method = RequestMethod.POST)
+    @ResponseBody
+    public void declineRequest(@RequestBody JsonNode json) {
+        JsonNode params = json.get("params");
+        String username = params.get("username").textValue();
+        String friendID = params.get("friendID").toString();
+
+        FriendsWithDao friendsWithDao = new FriendsWithDao();
+
+        int userID = userDAO.getBasedOnUsername(username).getUser_id();
+        FriendsWith friend = friendsWithDao.getByFriends(userID,Integer.valueOf(friendID));
+        friendsWithDao.delete(friend);
+    }
+
+    @RequestMapping(value = "/addFriend", method = RequestMethod.POST)
+    @ResponseBody
+    public void addFriend(@RequestBody JsonNode payload) {
+        JsonNode params = payload.get("params");
+        String username = params.get("username").textValue();
+        String friendID = params.get("friendID").toString();
+
+        User user = userDAO.getBasedOnUsername(username);
+        FriendImpl.sendRequest(user.getUser_id(),Integer.valueOf(friendID));
+    }
 
     @RequestMapping(value = "/getAllFriends", method = RequestMethod.GET)
     @ResponseBody
     public List<String> getAllFriends(@RequestParam String username) {
-        UserDao userDAO = new UserDao();
-        FriendsWithDao friendsDAO = new FriendsWithDao();
-
         User user = userDAO.getBasedOnUsername(username);
         List<User> friendsList = friendsDAO.getAllUserFriends(user.getUser_id());
 
         List<String> usernameList = new ArrayList<String>();
-
         for (User u: friendsList) {
             StringBuilder builder = new StringBuilder();
             builder.append(u.getUser_id());
@@ -37,8 +70,14 @@ public class FriendController {
             usernameList.add(builder.toString());
         }
 
-
-
         return usernameList;
+    }
+
+    @RequestMapping(value = "/getFriendRequests", method = RequestMethod.GET)
+    @ResponseBody
+    public List<User> getFriendRequests(@RequestParam String username) {
+        User user = userDAO.getBasedOnUsername(username);
+
+        return friendsDAO.getFriendRequests(user.getUser_id());
     }
 }
