e2fa2349eec3c5844c36da1fbf0470ce7c3ac6ec
Keyur Canji <Keyurschool8@gmail.com>
2020-10-02 15:11:21 +0100

    implemented a accept/decline function and icons

--- PigeonMessenger/src/main/frontend/src/Components/Messenger/FriendRequests/Request.jsx
+++ PigeonMessenger/src/main/frontend/src/Components/Messenger/FriendRequests/Request.jsx
@@ -1,7 +1,42 @@
 import React, { Component } from "react";
-
+import { FaTimes, FaCheck } from "react-icons/fa";
+import Axios from "axios";
 class Request extends Component {
   state = {};
+
+  acceptRequest = () => {
+    console.log("acceppt");
+    Axios.post("http://localhost:8080/acceptRequest", {
+      params: {
+        username: this.props.user,
+        friendID: this.props.id
+      }
+    }).then(
+      res => {
+        this.props.updateComponents();
+      },
+      err => {
+        console.log(JSON.stringify(err));
+      }
+    );
+  };
+
+  declineRequest = () => {
+    console.log("decline");
+    Axios.post("http://localhost:8080/declineRequest", {
+      params: {
+        username: this.props.user,
+        friendID: this.props.id
+      }
+    }).then(
+      res => {
+      
+      },
+      err => {
+        console.log(JSON.stringify(err));
+      }
+    );
+  };
 
   render() {
     return (
@@ -11,9 +46,11 @@ class Request extends Component {
             <div className="chat_ib">
               <h5>
                 {this.props.name}
-                <span
-                  className="chat_date"
-                >
+                <span onClick={this.acceptRequest} className="chat_date">
+                  <FaCheck />
+                </span>
+                <span onClick={this.declineRequest} className="chat_date">
+                  <FaTimes />
                 </span>
               </h5>
               {/* <p>{this.props.msg}</p> */}
