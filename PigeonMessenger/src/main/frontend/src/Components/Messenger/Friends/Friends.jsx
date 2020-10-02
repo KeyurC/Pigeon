d4f3795de321c9bdc069bf7b2864e427d8306204
Keyur Canji <Keyurschool8@gmail.com>
2020-10-02 15:13:50 +0100

    removed getFriends as it was moved to parent

--- PigeonMessenger/src/main/frontend/src/Components/Messenger/Friends/Friends.jsx
+++ PigeonMessenger/src/main/frontend/src/Components/Messenger/Friends/Friends.jsx
@@ -1,37 +1,11 @@
 import React, { Component } from "react";
 import Friend from "./Friend";
 
-import Axios from "axios";
-
 class Friends extends Component {
   
 
   componentDidMount() {
-    const user = this.props.user;
-
-    Axios.get("http://localhost:8080/getAllFriends", {
-      params: {
-        username: user
-      }
-    }).then(
-      res => {
-        let friends = res.data;
-        const friendsList = [];
-
-        for (let i = 0; i < friends.length; i++) {
-          let friend = friends[i].split(",");
-          let friendJson = {
-            id: friend[0],
-            name: friend[1]
-          };
-          friendsList.push(friendJson);
-        }
-        this.props.updateFriendsList(friendsList)
-      },
-      err => {
-        console.log(err);
-      }
-    );
+    this.props.reloadFriends();
   }
 
   returnFriendComponents() {
