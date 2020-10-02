a28c9032dd235f05ac0febaf7191948810af7d7d
Keyur Canji <Keyurschool8@gmail.com>
2020-10-02 15:07:11 +0100

    moved log4j configure method

--- PigeonMessenger/src/main/java/Spring/PigeonApp.java
+++ PigeonMessenger/src/main/java/Spring/PigeonApp.java
@@ -1,6 +1,6 @@
 package Spring;
 
-import Hibernate.HibernateUtil;
+import Utility.Log4jConf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PigeonApp {
     public static void main(String[] args) {
         SpringApplication.run(PigeonApp.class);
+        new Log4jConf();
     }
 }
