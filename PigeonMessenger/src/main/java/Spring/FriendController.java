0c60d420042cf52fe4a0f375f543e72730b0891e
Keyur Canji <Keyurschool8@gmail.com>
2020-10-15 20:37:03 +0100

    implemented websocket

--- PigeonMessenger/src/main/java/Spring/FriendController.java
+++ PigeonMessenger/src/main/java/Spring/FriendController.java
@@ -1,15 +1,18 @@
-package Controllers;
+package Spring;
 
 import DAO.DAOImpl.FriendsWithDao;
 import DAO.DAOImpl.UserDao;
 import Impl.FriendImpl;
 import Model.FriendsWith;
 import Model.User;
+import Session.UserSessionStore;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +22,26 @@ public class FriendController {
     private static UserDao userDAO = new UserDao();
     private static Logger logger = Logger.getLogger(FriendController.class);
 
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    public void updateActivityHub(String username) {
+        String sessionID = UserSessionStore.getSessionID(username);
+
+        if (sessionID != null) {
+            this.simpMessagingTemplate.convertAndSend("/friend/friendsList-"+sessionID,
+                    "true-"+sessionID);
+        }
+    }
+
+    @MessageMapping("/register")
+    public void checkFriendsList(JsonNode json) {
+        String sessionID = json.get("session").asText();
+        String username = json.get("username").asText();
+
+        UserSessionStore.add(sessionID,username);
+        UserSessionStore.print();
+    }
 
     @RequestMapping(value = "/acceptRequest", method = RequestMethod.POST)
     @ResponseBody
@@ -26,6 +49,10 @@ public class FriendController {
         JsonNode params = json.get("params");
         String username = params.get("username").textValue();
         String friendID = params.get("friendID").toString();
+
+        User friend = userDAO.get(Integer.valueOf(friendID));
+
+        this.updateActivityHub(friend.getUser_name());
 
         int userID = userDAO.getBasedOnUsername(username).getUser_id();
         FriendImpl.acceptRequest(userID,Integer.valueOf(friendID));
@@ -51,6 +78,10 @@ public class FriendController {
         JsonNode params = payload.get("params");
         String username = params.get("username").textValue();
         String friendID = params.get("friendID").toString();
+
+        User friend = userDAO.get(Integer.valueOf(friendID));
+
+        this.updateActivityHub(friend.getUser_name());
 
         User user = userDAO.getBasedOnUsername(username);
         FriendImpl.sendRequest(user.getUser_id(),Integer.valueOf(friendID));
@@ -78,7 +109,6 @@ public class FriendController {
     @ResponseBody
     public List<User> getFriendRequests(@RequestParam String username) {
         User user = userDAO.getBasedOnUsername(username);
-
         return friendsDAO.getFriendRequests(user.getUser_id());
     }
 }
